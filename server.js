a283680be8f135b72dc462f4a3476a2c7d5d7f85
Evhen-Zd <doctorsaiko@yahoo.com>
2019-10-25 15:42:04 +0300

    init project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const connectDb = require("./src/connection");
+
+const { PORT = '80' } = process.env
+const app = express()
+
+app.use((req, res, next) => {
+    res.send('Hello Jhony!')
+})
+
+app.listen(PORT)
